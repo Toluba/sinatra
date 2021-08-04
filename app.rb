b61933cc98c596b76176a51ac11333cb7afae2e2
toluba <tomikeolubamowo@aol.com>
2021-08-04 16:07:45 +0100

    cat form.erb completed

--- app.rb
+++ app.rb
@@ -1,15 +1,26 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
+require "rubygems"
+require "sinatra"
+require "sinatra/reloader" if development?
 
-get '/' do 
-  'Hello!'
-end 
+get "/" do
+  "Hello!"
+end
 
-get '/cat' do
+get "/random-cat" do
+  @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
 
+post "/named-cat" do
+  p params
+  @name = params[:name]
+  erb :index
+end
 
-get '/secret' do
-  'suwoop'
+get "/secret" do
+  "suwoop"
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
